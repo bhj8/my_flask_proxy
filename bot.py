ea9945e6b210a2c31152c8a919e4402dcd8954b9
鲍洪江 <baobao025@vip.qq.com>
2023-04-03 13:10:25 +0800

    1

--- bot.py
+++ bot.py
@@ -6,10 +6,37 @@ from api_openai import *
 from flask import Flask, make_response, render_template, request, url_for, jsonify
 from markupsafe import escape
 from dotenv import load_dotenv
+import logging
 
 
 load_dotenv()
 MY_PROXY_AES_KEY = os.getenv("MY_PROXY_AES_KEY")
+
+
+
+# 创建一个 logger
+logger = logging.getLogger('myapp')
+logger.setLevel(logging.DEBUG)
+
+# 创建一个控制台日志处理器 StreamHandler 并设置其日志级别
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.WARNING)
+
+# 创建一个文件日志处理器 FileHandler 并设置其日志级别
+file_handler = logging.FileHandler('myapp.log')
+file_handler.setLevel(logging.DEBUG)
+
+# 创建日志格式化器
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# 为控制台日志处理器和文件日志处理器分别设置格式化器
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+# 为 logger 添加控制台日志处理器和文件日志处理器
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
 
 app = Flask(__name__)
 
@@ -23,10 +50,12 @@ async def getpay():
             dic = json.loads(decrypted_message)
             result =  await get_response(dic)
             message = json.dumps(result)
-            encrypted_message = encrypt_message(message,MY_PROXY_AES_KEY)            
+            encrypted_message = encrypt_message(message,MY_PROXY_AES_KEY)    
+            logger.debug(f"encrypted_message: {encrypted_message}")        
             return jsonify({'code': 200, 'message': encrypted_message})
 
     print(f"bad pay info")
+    logger.warning(f"bad pay info")
     return make_response("", 400)
 
 @app.route('/', defaults={'path': ''})
